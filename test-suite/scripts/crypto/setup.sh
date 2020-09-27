e8f64d1841abdaeec4a43af889d182236d39285e
Chanaka Lakmal <ldclakmal@gmail.com>
2020-09-27 09:40:11 +0530

    Add crypto stdlib to test-suite

---
+++ test-suite/scripts/crypto/setup.sh
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/crypto/crypto.bal > src/crypto/crypto.bal
+curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/crypto/tests/crypto_test.bal > src/crypto/crypto_test.bal
