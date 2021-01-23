00e0caf3a4712bd2b38066febd64ffba8aea6a6a
Chanaka Lakmal <ldclakmal@gmail.com>
2021-01-23 14:26:34 +0530

    Add GitHub actions

--- test-suite/scripts/crypto/setup.sh
+++ test-suite/scripts/crypto/setup.sh
@@ -1,2 +1,2 @@
 curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/crypto/crypto.bal > crypto/crypto.bal
-curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/crypto/tests/crypto_test.bal > crypto/crypto_test.bal
+#curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/crypto/tests/crypto_test.bal > crypto/crypto_test.bal
