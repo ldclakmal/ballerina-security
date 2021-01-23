73b5dd70668cc58e7d0a2047997b9ae64551d3cb
Chanaka Lakmal <ldclakmal@gmail.com>
2021-01-23 14:26:26 +0530

    Update package paths

--- test-suite/scripts/encoding/setup.sh
+++ test-suite/scripts/encoding/setup.sh
@@ -1,2 +1,2 @@
-curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/url-encode-decode/url_encode_decode.bal > packages/encoding/encoding.bal
-curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/url-encode-decode/tests/url_encode_decode_test.bal > packages/encoding/encoding_test.bal
+curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/url-encode-decode/url_encode_decode.bal > encoding/encoding.bal
+curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/url-encode-decode/tests/url_encode_decode_test.bal > encoding/encoding_test.bal
