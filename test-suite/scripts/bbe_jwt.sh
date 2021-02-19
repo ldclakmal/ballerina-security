8255c14d797f841876a677f62925b3415205a790
Chanaka Lakmal <ldclakmal@gmail.com>
2021-02-19 11:53:26 +0530

    Add JWT issue/validate BBE

---
+++ test-suite/scripts/bbe_jwt.sh
@@ -0,0 +1,8 @@
+curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/jwt-issue-validate/jwt_issue_validate.bal > packages/bbe.jwt/jwt_issue_validate.bal
+curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/jwt-issue-validate/tests/jwt_issue_validate_test.bal > packages/bbe.jwt/tests/jwt_issue_validate_test.bal
+curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/jwt-issue-validate/jwt_mock_server.bal > packages/bbe.jwt/jwt_mock_server.bal
+
+sed -i 's+../resources+resources+g' packages/bbe.jwt/jwt_issue_validate.bal
+sed -i 's+../resources+resources+g' packages/bbe.jwt/jwt_mock_server.bal
+
+bal build packages/bbe.jwt
