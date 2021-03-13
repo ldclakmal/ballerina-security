d8b1f3db7b0b38b9e6214ee2503f773eb6c0245f
Chanaka Lakmal <ldclakmal@gmail.com>
2021-03-13 09:10:32 +0530

    Fix test suite BBEs

--- test-suite/scripts/bbe_jwt.sh
+++ test-suite/scripts/bbe_jwt.sh
@@ -1,6 +1,6 @@
-curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/jwt-issue-validate/jwt_issue_validate.bal > packages/bbe.jwt/jwt_issue_validate.bal
-curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/jwt-issue-validate/tests/jwt_issue_validate_test.bal > packages/bbe.jwt/tests/jwt_issue_validate_test.bal
-curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/jwt-issue-validate/jwt_mock_server.bal > packages/bbe.jwt/jwt_mock_server.bal
+curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/vswan-lake-alpha2/examples/jwt-issue-validate/jwt_issue_validate.bal > packages/bbe.jwt/jwt_issue_validate.bal
+curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/vswan-lake-alpha2/examples/jwt-issue-validate/tests/jwt_issue_validate_test.bal > packages/bbe.jwt/tests/jwt_issue_validate_test.bal
+curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/vswan-lake-alpha2/examples/jwt-issue-validate/jwt_mock_server.bal > packages/bbe.jwt/jwt_mock_server.bal
 
 sed -i 's+../resources+resources+g' packages/bbe.jwt/jwt_issue_validate.bal
 sed -i 's+../resources+resources+g' packages/bbe.jwt/jwt_mock_server.bal
