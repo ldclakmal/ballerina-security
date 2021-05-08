c4f8e5f2fa7d3d87ce64945f6221e2094ed02100
Chanaka Lakmal <ldclakmal@gmail.com>
2021-05-08 08:41:29 +0530

    Refactor test-suite for security BBEs

--- test-suite/scripts/bbe_crypto.sh
+++
@@ -1,6 +0,0 @@
-curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/crypto/crypto.bal > packages/bbe.crypto/crypto.bal
-curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/crypto/tests/crypto_test.bal > packages/bbe.crypto/tests/crypto_test.bal
-
-sed -i 's+../resource/path/to+resources+g' packages/bbe.crypto/crypto.bal
-
-bal build packages/bbe.crypto
