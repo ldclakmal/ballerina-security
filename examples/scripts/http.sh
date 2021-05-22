1f61ddc3f708dc2fb9b14fd138d7521b762fbad8
Chanaka Lakmal <ldclakmal@gmail.com>
2021-05-22 17:01:02 +0530

    Integrate all examples for test workflow

---
+++ examples/scripts/http.sh
@@ -0,0 +1,7 @@
+source ../../test-suite/scripts/validate.sh
+
+bal run http_service_with_jwt_auth.bal &
+sleep 10s
+response=$(bal run http_client_with_self_signed_jwt_auth.bal 2>&1 | tail -n 1)
+assertNotEmpty $response
+assertStatusCode $response
