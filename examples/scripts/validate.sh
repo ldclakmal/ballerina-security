2d5ad37b62cf6ab54ede0ab6a00a0ffc312797a8
Chanaka Lakmal <ldclakmal@gmail.com>
2021-05-22 18:39:48 +0530

    Improve validation scripts

--- examples/scripts/validate.sh
+++ examples/scripts/validate.sh
@@ -6,12 +6,16 @@ assertNotEmpty() {
 
 assertTextPayload() {
   if [[ "${1} ${2}" != "Hello, World!" ]]; then
+    echo "Expected: 'Hello, World!'"
+    echo "Actual: '${1} ${2}'"
     exit 1
   fi
 }
 
 assertJsonPayload() {
-  if [[ ${1} != "{\"data\":{\"bar\":\"Hello, World!\"}}" ]]; then
+  if [[ "${1} ${2}" != "{\"data\":{\"bar\":\"Hello, World!\"}}" ]]; then
+    echo "Expected: '{\"data\":{\"bar\":\"Hello, World!\"}}'"
+    echo "Actual: '${1} ${2}'"
     exit 1
   fi
 }
