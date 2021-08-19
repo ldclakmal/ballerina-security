7ae2a88529f08173f209407efddddd059dbd6735
Chanaka Lakmal <ldclakmal@gmail.com>
2021-08-19 11:11:03 +0530

    Add JWT bearer grant for BBE tests

---
+++ bbes/websocket/websocket_oauth2_jwt_bearer_grant.sh
@@ -0,0 +1,19 @@
+source assert.sh
+
+mkdir bbe
+curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/websocket-service-oauth2/websocket_service_oauth2.bal -o bbe/service.bal
+curl https://raw.githubusercontent.com/ballerina-platform/ballerina-distribution/master/examples/websocket-client-oauth2-jwt-bearern-grant-type/websocket_client_oauth2_jwt_bearer_grant_type.bal -o bbe/client.bal
+
+sed -i 's+../resource/path/to+resources+g' bbe/service.bal
+sed -i 's+../resource/path/to+resources+g' bbe/client.bal
+
+echo -e "\n--- Starting Ballerina STS ---"
+docker run --rm -p 9445:9445 --name ballerina-sts ldclakmal/ballerina-sts:latest &
+sleep 30s
+
+echo -e "\n--- Testing BBE ---"
+bal run bbe/service.bal &
+sleep 10s
+response=$(bal run bbe/client.bal 2>&1 | tail -n 1)
+assertNotEmpty "$response"
+assertEquals "$response" "Hello, World!"
